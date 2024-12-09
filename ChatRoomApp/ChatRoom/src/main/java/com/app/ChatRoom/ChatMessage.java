3d9b736af06d915c999fc076232580966e8f6dc1
Chirag Parmar <parmarchirag199800@gmail.com>
2024-12-09 18:49:03 +0530

    Initial commit for ChatRoomApp

---
+++ ChatRoomApp/ChatRoom/src/main/java/com/app/ChatRoom/ChatMessage.java
@@ -0,0 +1,36 @@
+package com.app.ChatRoom;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+
+
+public class ChatMessage {
+
+    private String sender;
+    private String content;
+
+    public ChatMessage(String sender, String content) {
+        this.sender = sender;
+        this.content = content;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public ChatMessage() {
+    }
+}
